379ae793d063f2ddf7bd95003772187382ab76a1
ippo <alla2634@edu.easj.dk>
2017-08-04 23:55:06 +0200

    lala

--- dev/dvlpmnt.js
+++ dev/dvlpmnt.js
@@ -1,13 +1,21 @@
 var $ = require("jQuery");
 FEED_URL = "https://www.youtube.com/feeds/videos.xml?channel_id=UClOf1XXinvZsy4wKPAkro2A";
 
-$.get(FEED_URL, function (data) {
-    $(data).find("entry").each(function () { // or "item" or whatever suits your feed
-        var el = $(this);
+$(function() {
+    var API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    var DEV_PLAYLIST_ID = 'PLAYFVhxsaqDtdUmvSddqTXF2_6qqIXXt-'; //
 
-        // console.log("----------------------------------------");
-        console.log("title       : " + el.find("title").text());
-        console.log("text        : " + el.find("text").text());
-        // console.log("Last Updated: " + el.find("LastUpdated").text());
+    var GOOGLE_API_URL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=id%2C+snippet%2C+contentDetails&playlistId='+DEV_PLAYLIST_ID+ '&key=' + API_KEY + '&callback=showVideos';
+
+    $.ajax({
+        url: GOOGLE_API_URL,
+        dataType: 'jsonp',
+        crossDomain: true
     });
+
+    window.showVideos = function(data) {
+        if (data.items && data.items.length > 0) {
+            $("#video").html('<iframe width="504" height="283" src="http://www.youtube.com/embed/'+data.items[0].contentDetails.videoId+'" frameborder="0" allowfullscreen></iframe>');
+        }
+    }
 });
