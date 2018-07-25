b25cc1eeac0a0e8e8e0f7b2edffdc868253f19e0
Allando: Polymorth <aln-nielsen@protonmail.com>
2018-07-25 16:58:58 +0200

    added to connection

--- kapbot/plugins/connectionUp.py
+++ kapbot/plugins/connectionUp.py
@@ -5,25 +5,25 @@ import os
 
 # Third-party library
 from disco.bot import Plugin
-
+from disco.gateway import GatewayClient
+from disco.util.logging import LoggingClass 
 
 class ConnectionUp(Plugin):
-    def __init__(self):
-
-
-    @Plugin.listen('MessageCreate')
     def main(self, event):
         while startUpLock is False:
             event.reply("")
             startUpLock = True
 
     def tatterTale(self):
-            startUpLock = False
-            host_os = os.name
-            host_user = ""
+        startUpLock = False
+        host_os = os.name
+        host_user = ""
 
-            host_sys_time = datetime.datetime.now()
+        host_sys_time = datetime.datetime.now()
 
         if host_os is 'posix':
             host_os = os.system("uname -a")
             host_user = os.system("whoami")
+
+    def listenForConnection(self):
+        pass
