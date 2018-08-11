8d08e9c9b2f50f5e3c6cc9a9d84456637804d9e5
Allando <aln-nielsen@protonmail.com>
2018-08-11 18:24:35 +0200

    tattleTale works

--- kapbot/plugins/connectionUp.py
+++ kapbot/plugins/connectionUp.py
@@ -7,32 +7,22 @@ import os
 from disco.bot import Plugin
 from disco.util.logging import LoggingClass
 
+
 class Connection(Plugin):
-    @Plugin.command("j")
-    def listenForConnection(self, event):
-        startUpLock = False
-
-        while startUpLock is False:
-            if LoggingClass.log() is 'Received HELLO, starting heartbeater...':
-                startUpLock = True
-                tattleTale()
-
-    @Plugin.command("h")
     def tattleTale(self, event):
-        host_os = os.name()
-        host_user = ""
+        host_info = str(os.uname())
+        host_time = datetime.datetime.now()
 
-        host_sys_time = datetime.datetime.now()
+        print(host_info)
+        print(host_time)
+        event.msg.reply("Uname: {}".format(host_info))
+        event.msg.reply("System time: {}".format(host_time))
 
-        if host_os is 'posix':
-            host_os = os.system("uname -a")
-            host_user = os.system("whoami")
+# conn = Connection
 
+# startUpLock = False
 
-            event.msg.reply(host_os)
-            event.msg.reply(host_user)
-            event.msg.reply(host_sys_time)
-
-    
-
-        
+# while startUpLock is False:
+#     if LoggingClass.log() is 'Received HELLO, starting heartbeater...':
+#         startUpLock = True
+#         conn.tattleTale()
