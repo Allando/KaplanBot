c39bb60f81e934fc30f1ed4b173b4cf80f461845
Allando <aln-nielsen@protonmail.com>
2018-12-19 18:52:09 +0100

    progress???

--- kapbot/reminder.py
+++ kapbot/reminder.py
@@ -1,6 +1,7 @@
 
 
 import datetime
+import json
 import threading
 
 class Memory(none):
@@ -14,20 +15,46 @@ class Memory(none):
 
 
 class reminder(none):
+	index = 0
 	"""Thiss class is for reminder feature"""
 	def create_memory(self, message_id, time):
 		mem = Memory(message_id, time)
+
+		mem = json.dumps({'message_id': mem.message_id, 'reminder_time': mem.time})
+
+		with open(pathToFile, "a") as fs:
+			fs.write(mem)
+
 		# TODO: Set a thread to manage the time for a memory
 
 	def read_memories(self):
-		pass
+		with open(pathToFile, "r") as fs:
+			memoriesObjList = fs.read()
+			memoriesList = []
+			for memory in memoriesObjList:
+				mem = Memory(memory['message_id'], memory['reminder_time'])
+				memoriesList.append(mem)
+			return memoriesList
 
-	def read_memory(self, id):
-		pass
+	def read_memory(self, _id):
+		with open(pathToFile, "r") as fs:
+			memoriesObjList = fs.read()
+			for memory in memoriesObjList:
+				if memory['message_id'] is _id:
+					mem = Memory(memory['message_id'], memory['reminder_time'])
+					return mem
 
-	def update_memory(self, id):
-		pass
+	def update_memory(self, message_id, time):
+		mem = Memory(message_id, time)
+		mem = json.dumps
+
+		with open(pathToFile, "r") as fr:
+			memoriesObjList = fs.read()
+			for memory in memoriesObjList:
+				if memory['message_id'] is _id:
+
+
 
 	def delete_memory(self, id):
 		pass
-		
+	
